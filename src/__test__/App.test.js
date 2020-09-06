e3e5c8842abb1aa02fc19061ecc0c9b6e3442a3b
karthick3018 <karthickraja3018@gmail.com>
2020-09-06 17:26:48 +0530

    first commit

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
-test('renders learn react link', () => {
+test('renders my name', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/karthick/i);
   expect(linkElement).toBeInTheDocument();
 });
