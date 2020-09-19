33d7a4bc83032b0675e87474430fcece88e3007a
karthick3018 <karthickraja3018@gmail.com>
2020-09-20 03:23:17 +0530

    event delegation checked

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const handleClick = (e) => {
+    console.log(e.target.id)
+  }
   return (
     <div>
      <p>Testing React components</p>
+     <div >
+       {/* event delegation */}
+       <ul onClick={handleClick}>
+         <li id="food">food</li>
+         <li id="dress">dress</li>
+         <li id="shoes">shoes</li>
+       </ul>
+     </div>
     </div>
   );
 }
